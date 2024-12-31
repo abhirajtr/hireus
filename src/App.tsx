4626434967cee7ecefd272360585cc440504d76e
Abhiraj T R <abhirajtr007@gmail.com>
2024-12-31 15:33:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+    </div>
+  )
+}
+
+export default App
